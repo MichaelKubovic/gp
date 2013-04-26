54f10ee2aba060ea674f6d37830b1d1034bb1893
Michael Kubovic <michael.kubovic@gmail.com>
2013-04-26 15:58:30 +0100

    Enables flags to be cliked and navigation appears.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log('works');
+$(document).ready(function() {
+
+});
+
+$('#top img').click(function() {
+	$img = $(this);
+	var country_clicked_id = $img.parent().attr('id');
+	var country_clicked = country_clicked_id.substr(0,3);
+
+	$('#' + country_clicked + '_nav').prev().hide().next().find('ul').fadeIn();
+});
