4673130ef559d26821f65cc8c2fadd2c5ac7656c
Michael Kubovic <michael.kubovic@gmail.com>
2013-04-27 18:53:32 +0100

    Adds home page, navigation rewritten, simplified html structure.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,36 @@
 $('#top img').click(function() {
-	$img = $(this);
-	var country_clicked_id = $img.parent().attr('id');
-	var country_clicked = country_clicked_id.substr(0,3);
+	var $img = $(this);
+	var country_to_open = $img.parent().hasClass('blg') ? 'blg' : 'svk';
 
-	$('#' + country_clicked + '_nav').prev().hide().next().find('ul').fadeIn();
+	$('.' + country_to_open + '.navigation').fadeIn().prev().hide();
 });
 
 $(window).on('load hashchange', function() {
 	load_page();
 });
 
-$content = $("#content");
-$sub = $("#subpage");
+var $content = $("#content");
+var $sub = $("#subpage");
+var requested_page;
+
 var load_page = function() {
 	var hash = window.location.hash;
 	$content.hide();
 	$sub.text('');
-	if(hash.length > 1) {
-		var requested_page = hash.substr(1);
+	if(hash.length > 1 && hash !== '#home') {
+		requested_page = hash.substr(1);
+	} else {
+		requested_page = 'home';
 
-		$.get('information/' + requested_page + '.html', function(response) {
-			// $sub.text($html.find('h2').text());
-			$content.html(response);
-			$content.fadeIn();
-
-			$sub.text($content.find('h2').text());
-		});
+		var hidden_flag = $("#top div:hidden");
+		hidden_flag.fadeIn().next().hide();
 	}
-}
+
+	$.get('information/' + requested_page + '.html', function(response) {
+		// $sub.text($html.find('h2').text());
+		$content.html(response);
+		$content.fadeIn();
+
+		$sub.text($content.find('h2').text());
+	});
+};
